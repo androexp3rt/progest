9d17d5b4abb1d34ebe1c8345822072dfe48c72cf
Sujeet Yadav <androexp3rt@gmail.com>
2024-10-09 23:14:25 +0530

    initial commit

--- src/app/(app)/managerDashboard/page.tsx
+++ src/app/(app)/managerDashboard/page.tsx
@@ -4,8 +4,7 @@ import { useSession } from "next-auth/react";
 
 export default function ManagerDashboard() {
   const { data: session } = useSession();
-  const user = session?.user;
-  const companyName = user.companyName;
+  const companyName: string | null = session?.user.companyName;
   const showUsers = () => {};
   const liClass = "w-full h-10 p-2 bg-[#C4A682] rounded-lg text-center";
   return (
@@ -13,7 +12,7 @@ export default function ManagerDashboard() {
       {/* sidebar */}
       <div className="w-60 h-full flex flex-col justify-start items-center px-2 py-5 space-y-5 bg-black text-white">
         <h1 className="max-sm:w-30 max-sm:text-lg w-full text-2xl text-center">
-          {companyName}
+          {companyName ?? "companyName"}
         </h1>
         <ul className="w-full flex flex-col space-y-2">
           <li className={liClass} onClick={showUsers}>
